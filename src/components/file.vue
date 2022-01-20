62c1b7f9f546e02635af660b610cb564210eb2a9
dolymood <dolymood@gmail.com>
2022-01-20 10:48:23 +0800

    fix emit

--- src/components/file.vue
+++ src/components/file.vue
@@ -193,7 +193,7 @@ export default {
           processResponse(args[2])
           return
         }
-        instance.ctx[`${event}`](args)
+        instance.ctx[`${event}`](...args)
       }
     }
     const fileProgress = () => {
